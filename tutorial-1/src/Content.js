6f31d4090d38c4a48a3d7a0173df6b604a226e0f
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-06-08 11:54:44 -0600

    feat: added usestate to handle name change and increment count

--- tutorial-1/src/Content.js
+++ tutorial-1/src/Content.js
@@ -1,21 +1,32 @@
+import { useState } from 'react';
+
 const Content = () => {
   /*
   * The `handleNameChange` function that returns a random name from an array. 
   * The function is then called and rendered by being placed between brackets. 
   * Every time the page is refrseshed a different name will be displayed.
   * */
+  const [name, setState] = useState('Daniel');
+  const [count, setCount] = useState(1);
 
   const handleNameChange = () => {
     const names =  ["Daniel", "Roberto", "Rocky"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setState(names[int]);
+  }
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
   }
 
   return (
     <main>
       <p>
-        Hello {handleNameChange()}!
+        Hello {name}!
       </p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Increment Count</button>
     </main>
   )
 }
