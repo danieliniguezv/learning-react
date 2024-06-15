23fb1de01fee7241a10fe83730da8b7340a84916
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-06-14 21:07:02 -0600

    feat: added title property and default title to header component

--- tutorial-1/src/App.js
+++ tutorial-1/src/App.js
@@ -8,10 +8,39 @@ function App() {
   * HTML in REACT. Code in between brackets is rendered as JavaScript.
   * The tags between the div tags are known as functional components.
   * */
+  /*
+  const [albums, setAlbums] = useState([
+    {
+      id: 1,
+      albumName: "Tragic Kingdom"
+    },
+    {
+      id: 2,
+      albumName: "Why Do They Rock So Hard?"
+    },
+    {
+      id: 3,
+      albumName: "Supernatural"
+    }
+  ]);
+
+  const handleAlbumRewrite = (id) => {
+    const newAlbumList = albums.map((album) => album.id === id ? {...album, 
+      albumName: "Cuando Los Ángeles Lloran"} : album);
+    setAlbums(newAlbumList);
+  }
+
+  const handleDeleteAlbum = (id) => {
+    const newAlbumList = albums.filter((album) => album.id !== id);
+    setAlbums(newAlbumList);
+  }
+  */
 
   return (
     <div className="App">
-      <Header />
+      <Header 
+        title="Albums"
+      />
       <Content />
       <Footer />
     </div>
